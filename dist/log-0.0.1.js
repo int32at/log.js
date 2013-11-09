a24430ef5b4070bca41b9a0476775f69d06dcacf
int32at <admin@andreas-scharf.at>
2013-11-09 22:27:08 +0100

    added formatting

--- dist/log-0.0.1.js
+++ dist/log-0.0.1.js
@@ -54,31 +54,44 @@
 	log = function() {
 		var self = this;
 		self.appender = log.appender || {};
+		self.format = "[{date}][{level}] {text}";
 
 		self.currentAppender = log.appender.consoleAppender;
+
+		var formatText = function(text, level) {
+			var date = new Date().toLocaleString();
+
+			var formattedText = log.format;
+			formattedText = formattedText.replace("{date}", date);
+			formattedText = formattedText.replace("{level}", level);
+			formattedText = formattedText.replace("{text}", text);
+
+			return formattedText;
+		};
 
 		return {
 
 			appender : self.appender,
+			format : self.format,
 
 			init : function(appender) {
 				self.currentAppender = appender;
 			},
 
 			info : function(text) {
-				self.currentAppender.info(text);
+				self.currentAppender.info(formatText(text, "INFO"));
 			},
 
 			warn : function(text) {
-				self.currentAppender.warn(text);
+				self.currentAppender.warn(formatText(text, "WARN"));
 			},
 
 			error : function(text) {
-				self.currentAppender.error(text);
+				self.currentAppender.error(formatText(text, "ERROR"));
 			},
 
 			debug : function(text) {
-				self.currentAppender.debug(text);
+				self.currentAppender.debug(formatText(text, "DEBUG"));
 			}
 		};
 	}();
