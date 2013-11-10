8d3e4167788e7a5e06655d36a9f8ce45400eb3ce
int32at <admin@andreas-scharf.at>
2013-11-10 21:01:20 +0100

    added grunt test

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,5 +60,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-jasmine');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.registerTask("test", ["jasmine"]);
 	grunt.registerTask('default', ['concat', 'uglify', 'jasmine']);
 };
