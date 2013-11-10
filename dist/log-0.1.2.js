dd4a5bf30faa7387ae0d1446df7e21d6698ca76d
int32at <admin@andreas-scharf.at>
2013-11-10 22:39:32 +0100

    implemented level restriction; now its possible to create your own logger!

--- dist/log-0.1.2.js
+++ dist/log-0.1.2.js
@@ -270,12 +270,21 @@
   log = window.log || {};
   
   log = function() {
-    var self = this;
-    self.appender = log.appender || {};
-    self.common = log.common || {};
-    self.format = "[{date}][{level}] {text}";
-
-    self.currentAppender = undefined;
+    var _this = this;
+    log.appender = log.appender || {};
+    log.common = log.common || {};
+    log.format = "[{date}][{level}] {text}";
+    _this.name = "log.js";
+    _this.currentLevel = 1;
+    _this.currentAppender = undefined;
+    _this.allLevels =  [
+                        { id: 0, name : "OFF"},
+                        { id: 1, name : "DEBUG"},
+                        { id: 2, name : "LOG"},
+                        { id: 3, name : "INFO"},
+                        { id: 4, name : "WARN"},
+                        { id: 5, name : "ERROR"}
+                      ];
 
     var formatText = function(text, level, args) {
       var date = new Date().toLocaleString();
@@ -284,7 +293,7 @@
 
       //format object as json to make it readable
       if(typeof text !== "string") {
-        text = "\n\r" + JSON.stringify(text, null, 2);
+        text = JSON.stringify(text, null, 2);
       }
 
       text = log.common.format(text, args);
@@ -294,40 +303,72 @@
       return formattedText;
     };
 
+    var findLevel = function(levelName) {
+      for (var i = _this.allLevels.length - 1; i >= 0; i--) {
+        var level = _this.allLevels[i];
+
+        if(level.name.toLowerCase() === levelName.toLowerCase())
+          return level;
+      }
+    };
+
     var init = function() {
-      self.currentAppender = log.appender.consoleAppender;
-      self.currentAppender.init();
+      _this.currentAppender = log.appender.consoleAppender;
+      _this.currentAppender.init();
     }();
 
-    return {
+    var executeLoggingMethod = function(level, text, args) {
+      if(level.id >= _this.currentLevel && _this.currentLevel !== 0)
+        _this.currentAppender[level.name.toLowerCase()](formatText(text, level.name, args));
+    };
 
-      appender : self.appender,
-      format : self.format,
-      common : self.common,
+    return {
+      level : function(levelName) {
+        if(typeof levelName !== "undefined") {
+          var level = findLevel(levelName);
+
+          if(typeof level !== "undefined")
+            _this.currentLevel = level.id;
+        }
+
+        return _this.allLevels[_this.currentLevel].name;
+      },
+
+      name : _this.name,
+      appender : log.appender,
+      format : log.format,
+      common : log.common,
 
       init : function(appender, args, callback) {
-        self.currentAppender = appender;
-        self.currentAppender.init(args, callback);
+        _this.currentAppender = appender;
+        _this.currentAppender.init(args, callback);
+      },
+
+      //logging methods
+      debug : function(text) {
+        executeLoggingMethod(_this.allLevels[1], text, arguments);
       },
 
       log : function(text) {
-        self.currentAppender.log(formatText(text, "LOG", arguments));
+        executeLoggingMethod(_this.allLevels[2], text, arguments);
       },
 
       info : function(text) {
-        self.currentAppender.info(formatText(text, "INFO", arguments));
+        executeLoggingMethod(_this.allLevels[3], text, arguments);
       },
 
       warn : function(text) {
-        self.currentAppender.warn(formatText(text, "WARN", arguments));
+        executeLoggingMethod(_this.allLevels[4], text, arguments);
       },
 
       error : function(text) {
-        self.currentAppender.error(formatText(text, "ERROR", arguments));
+        executeLoggingMethod(_this.allLevels[5], text, arguments);
       },
 
-      debug : function(text) {
-        self.currentAppender.debug(formatText(text, "DEBUG", arguments));
+      get : function(name) {
+        var x = this;
+        x.name = name;
+        return x;
       }
     };
   }();
