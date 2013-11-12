1e7e99af849da78181d103bb5c6fc544968d6d29
int32at <admin@andreas-scharf.at>
2013-11-12 20:05:42 +0100

    added implementation of status appender

--- src/appender/spStatusAppender.js
+++ src/appender/spStatusAppender.js
@@ -6,30 +6,42 @@
     
     var _args;
 
+    var _sendMessage = function(level, text, color) {
+      var status = SP.UI.Status.addStatus(":", text);
+      SP.UI.Status.setStatusPriColor(status, color);
+      setTimeout(function () { SP.UI.Status.removeStatus(status); }, _args.timeout);
+    };
+
     return {
 
       init : function(args) {
         _args = args;
+
+        if(typeof _args === "undefined")
+          _args = {};
+
+        if(typeof _args.timeout === "undefined")
+          _args.timeout = 5000; //5 sec
       },
 
       log : function(text) {
-        alert(text);
+        _sendMessage(text, "blue");
       },
 
       info : function(text) {
-        alert(text);
+        _sendMessage(text, "blue");
       },
 
       warn : function(text) {
-        alert(text);
+        _sendMessage(text, "yellow");
       },
 
       error : function(text) {
-        alert(text);
+        _sendMessage(text, "red");
       },
 
       debug : function(text) {
-        alert(text);
+        _sendMessage(text, "blue");
       }
     };
   }();
