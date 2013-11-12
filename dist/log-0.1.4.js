d086fd2c319abea2c673b70375036ce384914975
int32at <admin@andreas-scharf.at>
2013-11-12 20:36:16 +0100

    implemented notification appender

--- dist/log-0.1.4.js
+++ dist/log-0.1.4.js
@@ -261,6 +261,52 @@ var logger = (function(name, appender){
   logger = window.logger || {};
   logger.appender = logger.appender || {};
   
+  logger.appender.spNotifyAppender = function() {
+    
+    var _args;
+
+    var _sendMessage = function(text) {
+      var notify = SP.UI.Notify.addNotification(text);
+      setTimeout(function () { SP.UI.Notify.removeNotification(notify) }, _args.timeout);
+    };
+
+    return {
+
+      init : function(args) {
+        _args = args;
+
+        _args = _args || {};
+
+        if(typeof _args.timeout === "undefined")
+          _args.timeout = 3000; //3 sec
+      },
+
+      log : function(text) {
+        _sendMessage(text);
+      },
+
+      info : function(text) {
+        _sendMessage(text);
+      },
+
+      warn : function(text) {
+        _sendMessage(text);
+      },
+
+      error : function(text) {
+        _sendMessage(text);
+      },
+
+      debug : function(text) {
+        _sendMessage(text);
+      }
+    };
+  }();
+}());
+(function() {
+  logger = window.logger || {};
+  logger.appender = logger.appender || {};
+  
   logger.appender.spStatusAppender = function() {
     
     var _args;
