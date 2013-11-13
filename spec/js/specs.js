4f53de3479aa0ab9a90cb608bbedf45282a9f749
int32at <admin@andreas-scharf.at>
2013-11-13 20:45:50 +0100

    added instance test

--- spec/js/specs.js
+++ spec/js/specs.js
@@ -1,7 +1,12 @@
 (function() {
   describe('logger.coffee specs', function() {
-    return it('should be possible to use the logger object', function() {
+    it('should be possible to use the logger object', function() {
       return expect(logger).not.toBeUndefined();
+    });
+    return it('should be possible to instance a new logger object', function() {
+      var myLogger;
+      myLogger = new logger("My Logger");
+      return expect(myLogger).not.toBeUndefined();
     });
   });
 
--- spec/loggerSpec.coffee
+++ spec/loggerSpec.coffee
@@ -2,3 +2,8 @@ describe 'logger.coffee specs', ->
   
   it 'should be possible to use the logger object', ->
     expect(logger).not.toBeUndefined()
+
+  it 'should be possible to instance a new logger object', ->
+    myLogger = new logger("My Logger")
+
+    expect(myLogger).not.toBeUndefined()
