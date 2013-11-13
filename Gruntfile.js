593fce7a2f21f3237ac001923a5fb4951c4386f5
int32at <admin@andreas-scharf.at>
2013-11-13 20:33:08 +0100

    initial checkin of coffee source

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    coffee: {
+      compile: {
+        files: {
+          "dist/<%= pkg.name %>-<%= pkg.version %>.js": ["src/logger.coffee"],
+          'spec/js/specs.js' : 'spec/*Spec.coffee'
+        }
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: "/*! <%= pkg.name %> <%= grunt.template.today('dd-mm-yyyy') %> */\n"
+      },
+      dist: {
+        files: {
+          "dist/<%= pkg.name %>-<%= pkg.version %>.min.js" : ["dist/<%= pkg.name %>-<%= pkg.version %>.js"]
+        }
+      }
+    },
+
+    jasmine: {
+      pivotal: {
+        src: 'dist/<%= pkg.name %>-<%= pkg.version %>.js',
+        options: {
+          vendor: 'lib/*.js',
+          keepRunner: true,
+          specs: 'spec/js/specs.js',
+          '--web-security' : false,
+          '--local-to-remote-url-access' : true,
+          '--ignore-ssl-errors' : true
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-coffee");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.registerTask("default", ["coffee", "uglify", "jasmine"]);
+};
